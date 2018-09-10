15bba9e6257b78b5c736219a90bed152126e5a5d
aidon0818 <30116992@qq.com>
2018-09-10 18:01:47 +0800

    修改controller类获取参数

--- src/main/java/com/example/elasticsearch/controller/ElasticSearchController.java
+++ src/main/java/com/example/elasticsearch/controller/ElasticSearchController.java
@@ -5,33 +5,63 @@ import com.example.elasticsearch.dao.EmployeeRepository;
 import com.example.elasticsearch.model.Employee;
 import com.example.elasticsearch.model.Entity;
 import com.example.elasticsearch.service.CityESService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/es")
 public class ElasticSearchController {
     @Autowired
-    private EmployeeRepository employeeRepository;
-    @Autowired
-    private CityESService esService;
+    private EmployeeRepository er;
 
-    @RequestMapping("/add")
-    public String add() {
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+
+    public String add(@RequestBody Map<String,String> req) {
         Employee employee = new Employee();
-        employee.setId("1");
-        employee.setFirstName("xuxu");
+        employee.setId(req.get("id"));
+        employee.setFirstName(req.get("name"));
         employee.setLastName("zh");
         employee.setAge(26);
         employee.setAbout("i am in peking");
-        employeeRepository.save(employee);
+        er.save(employee);
         return "success";
     }
 
-    @RequestMapping("/add2")
-    public void add2() {
-        Entity entity=new Entity(Long.valueOf(1),"test");
-        esService.saveEntity(entity);
+    //删除
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable("id")String id) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        er.delete(employee);
+        return "success";
     }
+
+    //局部更新
+    @RequestMapping("/update")
+    public String update() {
+
+        Employee employee = er.queryEmployeeById("1");
+        employee.setFirstName("哈哈");
+        er.save(employee);
+
+        System.err.println("update a obj");
+
+        return "success";
+    }
+
+    //查询
+    @RequestMapping("/query")
+    public Employee query() {
+
+        Employee accountInfo = er.queryEmployeeById("1");
+        System.err.println(new Gson().toJson(accountInfo));
+
+        return accountInfo;
+    }
+
 }
